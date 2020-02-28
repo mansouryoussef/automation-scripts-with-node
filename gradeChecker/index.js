04837064d7b4d7974033740f599e0a74dffb1412
Youssef Mansour <Contact@youssef.fi>
2020-02-28 14:14:29 +0200

    Added: cron package & set up CronJob

--- gradeChecker/index.js
+++ gradeChecker/index.js
@@ -1,41 +1,14 @@
-const connect = require('./config/db');
-const scraper = require('./utils/scraper');
-const sendEmail = require('./utils/sendEmail');
-const compareCoursesByGrade = require('./utils/compareCoursesByGrade');
-const { getAllCourses, updateDatabase } = require('./utils/mongoDB');
-const createHTMLFromCoursesArray = require('./utils/createHTMLFromCoursesArray');
+const CronJob = require('cron').CronJob;
 
-(async () => {
-	await connect();
+const checkGrades = require('./utils/checkGrades');
 
-	const vamkCourseData = await scraper();
-	const coursesInDatabase = await getAllCourses();
-
-	// Remove total credits from vamkCourseData array & save it in a variable.
-	const totalCredits = vamkCourseData.shift().totalCr;
-
-	const changedCourses = compareCoursesByGrade(
-		vamkCourseData,
-		coursesInDatabase
-	);
-
-	if (changedCourses.length !== 0) {
-		console.log('There is an update!');
-
-		await updateDatabase(changedCourses);
-
-		const mail = {
-			from: 'hello@youssef.fi',
-			to: 'contact@youssef.fi',
-			subject: 'Course update!',
-			html: createHTMLFromCoursesArray(changedCourses)
-		};
-
-		await sendEmail(mail);
-	} else {
-		console.log(`No change in coures!
-Current cr count: ${totalCredits}`);
+const job = new CronJob('0 8-19/2 * * 1-5', () => {
+	// Will excute this code every 2 hoiurs between 8:00 - 19:00 and only on Monday to Friday
+	try {
+		checkGrades();
+	} catch (error) {
+		console.log(`There was an error! ${{ error }}`);
 	}
+});
 
-	process.exit();
-})();
+job.start();
